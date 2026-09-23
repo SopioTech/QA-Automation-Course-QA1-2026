24da3c120d4ba1b0344b78a57917322c974ff085
Sopio <sopiotkhvani@gmail.com>
2026-09-23 00:59:06 -0400

    homework 4

---
+++  Homework-Javascript/Homework-4-numbers/numbers.js
@@ -0,0 +1,52 @@
+let age = 39;
+let price = 10.9;
+let temperature = -3;
+
+console.log(age);
+console.log(price);
+console.log(temperature);
+
+console.log(5 + 4);
+console.log(5 - 4);
+console.log(6 * 5);
+console.log(6 / 2);
+console.log(5 % 2);
+
+console.log(5 > 3);
+console.log(4 === 4);
+console.log(Math.round(5.7));
+console.log(Math.floor(5.7));
+console.log(Math.ceil(5.5));
+console.log(Math.trunc(5.5));
+
+// .toFixed() - Formats 
+
+let bookPrice = 101.444
+
+console.log(price.toFixed(1));
+
+// converting numbers
+
+console.log (Number("Fourtytwo"));
+console.log(Number("10"));
+console.log(Number("number"));
+
+// Converting Numbers
+
+console.log(parseInt("10.5"));
+
+//checking numbers
+
+console.log(isNaN("hello"));
+
+console.log(Number.isInteger(10));
+
+console.log(Math.abs(-1));
+console.log(Math.abs(1));
+console.log(Math.abs(0));
+
+console.log(Math.min(10,5,6,1,2));
+console.log(Math.max(19,2,1,33));
+console.log(Math.pow(3, 3));
+console.log(Math.sqrt(30));
+console.log(Math.random());
